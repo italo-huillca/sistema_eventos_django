e76dbe75e0a08d0d90bf45157780eb48a086ae6d
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-01 18:13:44 -0500

    feat: añadiendo crud para registrar usuario a evento

--- eventos/views.py
+++ eventos/views.py
@@ -11,3 +11,9 @@ def crear_evento(request):
     else:
         form = EventoForm()
     return render(request, 'eventos/crear_evento.html', {'form': form})
+def registrar_en_evento(request, evento_id):
+    evento = Evento.objects.get(id=evento_id)
+    if request.method == 'POST':
+        registro, created = RegistroEvento.objects.get_or_create(usuario=request.user, evento=evento)
+        return redirect('detalle_evento', evento_id=evento_id)
+    return render(request, 'eventos/registrar_en_evento.html', {'evento': evento})
