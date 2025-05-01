1e88809e0714121c4cdf52dbb7efb5b003174401
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-01 18:13:12 -0500

    feat: añadiendo crud para crear evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Evento, RegistroEvento
+from .forms import EventoForm, RegistroEventoForm
 
-# Create your views here.
+def crear_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_eventos')
+    else:
+        form = EventoForm()
+    return render(request, 'eventos/crear_evento.html', {'form': form})
